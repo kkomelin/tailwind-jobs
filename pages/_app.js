6066ce9cf8d4f8dda79d0fd6391d2bef7b180815
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-05-17 15:52:54 +0300

    Improved default meta-viewport setting and set html language to English.

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
